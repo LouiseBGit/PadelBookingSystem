61b221791ca10ee6358d6b7da1434b837ca9e8cd
LouiseBGit <lowalis91@gmail.com>
2026-05-19 14:36:51 +0200

    Add properties to Customer.cs

--- PadelBooking.Core/Models/Customer.cs
+++ PadelBooking.Core/Models/Customer.cs
@@ -8,5 +8,11 @@ namespace PadelBooking.Core.Models
 {
     internal class Customer
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
     }
 }
