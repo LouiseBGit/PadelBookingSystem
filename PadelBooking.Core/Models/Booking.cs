30888120deabf60b877cb20edc6703351bb64247
LouiseBGit <lowalis91@gmail.com>
2026-05-19 14:58:50 +0200

    Add properties in Booking.cs

--- PadelBooking.Core/Models/Booking.cs
+++ PadelBooking.Core/Models/Booking.cs
@@ -8,5 +8,10 @@ namespace PadelBooking.Core.Models
 {
     internal class Booking
     {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public int CourtNumber { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; } = null!;
     }
 }
