c7cf67cd391af1c13c097d47a79772ba0b5699b0
LouiseBGit <lowalis91@gmail.com>
2026-05-20 14:33:55 +0200

    Added interface for customers

--- PadelBooking.Core/Interfaces/ICustomerRepository.cs
+++ PadelBooking.Core/Interfaces/ICustomerRepository.cs
@@ -8,8 +8,24 @@ using PadelBooking.Core.Models;
 namespace PadelBooking.Core.Interfaces
 {
     //interface -vad som ska göras, men inte hur det görs (som ett kontrakt)
+    //metoderna som måste finnas för att hantera bokning
+    //Task = asynkron metod --> annat kan göras undertiden
     public interface ICustomerRepository
     {
+        //hämta alla kunder från databasen
+        //returnera som en lista
+        Task<List<Customer>> GetAllCustomersAsync();
+        //hämta en kund med ID
+        //? = null om ingen kund hittas med det ID
+        Task<Customer?> GetCustomerByIdAsync(int id);
+        //lägg till kund
+        Task AddAsync(Customer customer);
+        //uppdatera kund
+        Task UpdateAsync(Customer customer);
+        //ta bort kund med ID
+        Task DeleteAsync(int id);
+
+
 
     }
 }
