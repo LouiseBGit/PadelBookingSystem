63f8f5d937c682f20eff07c526c6c0ed375f5394
LouiseBGit <lowalis91@gmail.com>
2026-05-20 14:00:24 +0200

    Added Interface and Repository with classes

---
+++ PadelBooking.Core/Interfaces/IBookingRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadelBooking.Core.Interfaces
+{
+    //interface -vad som ska göras, men inte hur det görs (som ett kontrakt)
+    internal class IBookingRepository
+    {
+    }
+}
