bf9016136a421f9b71e2a1332d007e0b55670fb2
LouiseBGit <lowalis91@gmail.com>
2026-05-29 12:35:03 +0200

    Added DTO classes för cleaner API-response

---
+++ PadelBooking.Core/DTOs/CreateBookingDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadelBooking.Core.DTOs
+{
+    /// <summary>
+    /// DTO för att skapa en bokning
+    /// </summary>
+    public class CreateBookingDto
+    {
+        //banan som ska bokas
+        public int CourtNumber { get; set; }
+        //Starttid för bokningen
+        public DateTime StartTime { get; set; }
+    }
+}
---
+++ PadelBooking.Core/DTOs/UpdateBookingDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadelBooking.Core.DTOs
+{
+    /// <summary>
+    /// DTO för att uppdatera en bokning
+    /// </summary>
+    public class UpdateBookingDto
+    {
+        //det nya numret på banan
+        public int CourtNumber { get; set; }
+        //den nya starttiden för bokningen
+        public DateTime StartTime { get; set; }
+    }
+}
