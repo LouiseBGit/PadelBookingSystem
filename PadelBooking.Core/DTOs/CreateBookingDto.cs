c3364bfca226c2d318985413bb0dcca02939014c
LouiseBGit <lowalis91@gmail.com>
2026-06-16 11:14:45 +0200

    Add annotation

--- PadelBooking.Core/DTOs/CreateBookingDto.cs
+++ PadelBooking.Core/DTOs/CreateBookingDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,13 @@ namespace PadelBooking.Core.DTOs
     public class CreateBookingDto
     {
         //banan som ska bokas
+        [Range(1, 3, ErrorMessage = "Numret på banan måste vara mellan 1-3")]
         public int CourtNumber { get; set; }
         //Starttid för bokningen
+        [Required(ErrorMessage = "Starttid måste anges")]
         public DateTime StartTime { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Id måste vara större än 0")]
         public int CustomerId { get; set; }
     }
 }
