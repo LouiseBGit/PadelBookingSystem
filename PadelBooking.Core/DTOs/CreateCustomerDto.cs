59b1561c5258103c4203328daaf24ba99b41269e
LouiseBGit <lowalis91@gmail.com>
2026-06-23 13:49:53 +0200

    Added more annotations

--- PadelBooking.Core/DTOs/CreateCustomerDto.cs
+++ PadelBooking.Core/DTOs/CreateCustomerDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,15 @@ namespace PadelBooking.Core.DTOs
     /// </summary>
     public class CreateCustomerDto
     {
+        [Required]
         public string FirstName { get; set; } = string.Empty;
+        [Required]
         public string LastName { get; set; } = string.Empty;
+        [Required]
+        [Phone]
         public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty; 
 
 
--- PadelBooking.Core/DTOs/UpdateCustomerDto.cs
+++ PadelBooking.Core/DTOs/UpdateCustomerDto.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,13 @@ namespace PadelBooking.Core.DTOs
     /// </summary>
     public class UpdateCustomerDto
     {
+        [Required]
         public string FirstName { get; set; } = string.Empty;
+        [Required]
         public string LastName { get; set; } = string.Empty;
+        [Phone]
         public string PhoneNumber { get; set; } = string.Empty;
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
 
     }
